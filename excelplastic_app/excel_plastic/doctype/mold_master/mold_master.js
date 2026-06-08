5086ce9880d062f87b3ed1be9dfb0e5675a868ff
rajesh-kumar-20 <rajeshkumarmech77@gmail.com>
2026-06-08 09:55:37 +0000

    custom doctype moved to app

---
+++ excelplastic_app/excel_plastic/doctype/mold_master/mold_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Rajesh Kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mold Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
